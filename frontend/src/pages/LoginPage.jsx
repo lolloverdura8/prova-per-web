aea6f6e620450c03b08d6c835059d0b5b809ccc0
lolloverdura8 <lorenzoverdura8@gmail.com>
2025-02-27 19:22:38 +0100

    tolto alert login con successo

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -38,7 +38,6 @@ const LoginPage = () => {
                     localStorage.setItem('token', result.token);
                     setUser(result.user);
                     navigate('/home')
-                    alert('Login effettuato con successo!');
                 }
             } else {
                 alert(result.message || 'Errore durante l\'operazione');
