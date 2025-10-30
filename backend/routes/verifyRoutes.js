eb148664b1806bc2accb5cfc45e58feb8685de5c
lolloverdura8 <lorenzoverdura8@gmail.com>
2025-10-30 17:24:46 +0100

    email di verifica2

--- backend/routes/verifyRoutes.js
+++ backend/routes/verifyRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/userModel");
 
 router.get("/verify-email", async (req, res) => {
     try {
